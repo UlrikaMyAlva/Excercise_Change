5cc3727af722d409812bad9ee65de142b9a899ca
Ulrika Eriksson <ulrika.m.a.eriksson@gmail.com>
2023-02-06 10:58:43 +0100

    Initial commit

---
+++ WindowsFormsVariant Inlämningsuppgift 1/Inlämningsuppgift1/Inlämningsuppgift1/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Inlämningsuppgift1
+{
+    partial class Växel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPris = new System.Windows.Forms.Label();
+            this.lblBetalt = new System.Windows.Forms.Label();
+            this.lblVäxel = new System.Windows.Forms.Label();
+            this.txbPris = new System.Windows.Forms.TextBox();
+            this.txbBetalt = new System.Windows.Forms.TextBox();
+            this.Beräkna = new System.Windows.Forms.Button();
+            this.tbxFemhundra = new System.Windows.Forms.TextBox();
+            this.tbxHundra = new System.Windows.Forms.TextBox();
+            this.tbxFemtio = new System.Windows.Forms.TextBox();
+            this.tbxTio = new System.Windows.Forms.TextBox();
+            this.tbxFem = new System.Windows.Forms.TextBox();
+            this.tbxEn = new System.Windows.Forms.TextBox();
+            this.tbxTjugo = new System.Windows.Forms.TextBox();
+            this.tbxTvåhundra = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblPris
+            // 
+            this.lblPris.AutoSize = true;
+            this.lblPris.Location = new System.Drawing.Point(69, 61);
+            this.lblPris.Name = "lblPris";
+            this.lblPris.Size = new System.Drawing.Size(24, 13);
+            this.lblPris.TabIndex = 0;
+            this.lblPris.Text = "Pris";
+            // 
+            // lblBetalt
+            // 
+            this.lblBetalt.AutoSize = true;
+            this.lblBetalt.Location = new System.Drawing.Point(69, 94);
+            this.lblBetalt.Name = "lblBetalt";
+            this.lblBetalt.Size = new System.Drawing.Size(34, 13);
+            this.lblBetalt.TabIndex = 1;
+            this.lblBetalt.Text = "Betalt";
+            // 
+            // lblVäxel
+            // 
+            this.lblVäxel.AutoSize = true;
+            this.lblVäxel.Location = new System.Drawing.Point(72, 152);
+            this.lblVäxel.Name = "lblVäxel";
+            this.lblVäxel.Size = new System.Drawing.Size(33, 13);
+            this.lblVäxel.TabIndex = 3;
+            this.lblVäxel.Text = "Växel";
+            // 
+            // txbPris
+            // 
+            this.txbPris.Location = new System.Drawing.Point(205, 54);
+            this.txbPris.Name = "txbPris";
+            this.txbPris.Size = new System.Drawing.Size(100, 20);
+            this.txbPris.TabIndex = 4;
+            this.txbPris.TextChanged += new System.EventHandler(this.txbPris_TextChanged);
+            // 
+            // txbBetalt
+            // 
+            this.txbBetalt.Location = new System.Drawing.Point(205, 94);
+            this.txbBetalt.Name = "txbBetalt";
+            this.txbBetalt.Size = new System.Drawing.Size(100, 20);
+            this.txbBetalt.TabIndex = 5;
+            this.txbBetalt.TextChanged += new System.EventHandler(this.txbBetalt_TextChanged);
+            // 
+            // Beräkna
+            // 
+            this.Beräkna.Location = new System.Drawing.Point(271, 243);
+            this.Beräkna.Name = "Beräkna";
+            this.Beräkna.Size = new System.Drawing.Size(91, 60);
+            this.Beräkna.TabIndex = 6;
+            this.Beräkna.Text = "Beräkna";
+            this.Beräkna.UseVisualStyleBackColor = true;
+            this.Beräkna.Click += new System.EventHandler(this.Beräkna_Click);
+            // 
+            // tbxFemhundra
+            // 
+            this.tbxFemhundra.Location = new System.Drawing.Point(72, 168);
+            this.tbxFemhundra.Name = "tbxFemhundra";
+            this.tbxFemhundra.Size = new System.Drawing.Size(127, 20);
+            this.tbxFemhundra.TabIndex = 7;
+            this.tbxFemhundra.TextChanged += new System.EventHandler(this.txbFemhundra_TextChanged);
+            // 
+            // tbxHundra
+            // 
+            this.tbxHundra.Location = new System.Drawing.Point(72, 243);
+            this.tbxHundra.Name = "tbxHundra";
+            this.tbxHundra.Size = new System.Drawing.Size(127, 20);
+            this.tbxHundra.TabIndex = 8;
+            // 
+            // tbxFemtio
+            // 
+            this.tbxFemtio.Location = new System.Drawing.Point(72, 283);
+            this.tbxFemtio.Name = "tbxFemtio";
+            this.tbxFemtio.Size = new System.Drawing.Size(127, 20);
+            this.tbxFemtio.TabIndex = 9;
+            // 
+            // tbxTio
+            // 
+            this.tbxTio.Location = new System.Drawing.Point(72, 356);
+            this.tbxTio.Name = "tbxTio";
+            this.tbxTio.Size = new System.Drawing.Size(127, 20);
+            this.tbxTio.TabIndex = 10;
+            // 
+            // tbxFem
+            // 
+            this.tbxFem.Location = new System.Drawing.Point(72, 392);
+            this.tbxFem.Name = "tbxFem";
+            this.tbxFem.Size = new System.Drawing.Size(127, 20);
+            this.tbxFem.TabIndex = 11;
+            // 
+            // tbxEn
+            // 
+            this.tbxEn.Location = new System.Drawing.Point(72, 430);
+            this.tbxEn.Name = "tbxEn";
+            this.tbxEn.Size = new System.Drawing.Size(127, 20);
+            this.tbxEn.TabIndex = 12;
+            // 
+            // tbxTjugo
+            // 
+            this.tbxTjugo.Location = new System.Drawing.Point(72, 319);
+            this.tbxTjugo.Name = "tbxTjugo";
+            this.tbxTjugo.Size = new System.Drawing.Size(127, 20);
+            this.tbxTjugo.TabIndex = 13;
+            // 
+            // tbxTvåhundra
+            // 
+            this.tbxTvåhundra.Location = new System.Drawing.Point(72, 205);
+            this.tbxTvåhundra.Name = "tbxTvåhundra";
+            this.tbxTvåhundra.Size = new System.Drawing.Size(124, 20);
+            this.tbxTvåhundra.TabIndex = 14;
+            this.tbxTvåhundra.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Växel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 490);
+            this.Controls.Add(this.tbxTvåhundra);
+            this.Controls.Add(this.tbxTjugo);
+            this.Controls.Add(this.tbxEn);
+            this.Controls.Add(this.tbxFem);
+            this.Controls.Add(this.tbxTio);
+            this.Controls.Add(this.tbxFemtio);
+            this.Controls.Add(this.tbxHundra);
+            this.Controls.Add(this.tbxFemhundra);
+            this.Controls.Add(this.Beräkna);
+            this.Controls.Add(this.txbBetalt);
+            this.Controls.Add(this.txbPris);
+            this.Controls.Add(this.lblVäxel);
+            this.Controls.Add(this.lblBetalt);
+            this.Controls.Add(this.lblPris);
+            this.Name = "Växel";
+            this.Text = "Växel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPris;
+        private System.Windows.Forms.Label lblBetalt;
+        private System.Windows.Forms.Label lblVäxel;
+        private System.Windows.Forms.TextBox txbPris;
+        private System.Windows.Forms.TextBox txbBetalt;
+        private System.Windows.Forms.Button Beräkna;
+        private System.Windows.Forms.TextBox tbxFemhundra;
+        private System.Windows.Forms.TextBox tbxHundra;
+        private System.Windows.Forms.TextBox tbxFemtio;
+        private System.Windows.Forms.TextBox tbxTio;
+        private System.Windows.Forms.TextBox tbxFem;
+        private System.Windows.Forms.TextBox tbxEn;
+        private System.Windows.Forms.TextBox tbxTjugo;
+        private System.Windows.Forms.TextBox tbxTvåhundra;
+    }
+}
+
---
+++ WindowsFormsVariant Inlämningsuppgift 1/Inlämningsuppgift1/Inlämningsuppgift1/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inlämningsuppgift1
+{
+    public partial class Växel : Form
+    {
+        public Växel()
+        {
+            InitializeComponent();
+        }
+
+        private void Beräkna_Click(object sender, EventArgs e)
+        {
+            //INPUT
+            //Här skriver användaren priset på varan, vad den betalar, sedan räknas växeln ut. 
+            int pris = int.Parse(txbPris.Text);
+            int betalt = int.Parse(txbBetalt.Text);
+            int växel = betalt - pris;
+
+
+            //UTRÄKNINGEN AV VÄXEL
+            //Växeln delas på 500. Denna siffra sparas och skrivs sedan ut i textboxen Utdata. Pengarna dras sedan från växeln, och en ny int skapas med det som är kvar av växeln. 
+            int femhundraLappar = växel / 500;
+            int växelTvåhundraLappar = växel - (femhundraLappar * 500);
+
+            //Ovan upprepas, men nu med hundralappar. Samt med den kvarvarande växeln, efter att femhundralapparna plockats bort. Detta upprepas i samtliga valörer. 
+            int tvåhundraLappar = växelTvåhundraLappar / 200;
+            int växelHundraLappar = växelTvåhundraLappar - (tvåhundraLappar * 200);
+
+            int hundraLappar = växelHundraLappar / 100;
+            int växelFemtioLappar = växelHundraLappar - (hundraLappar * 100);
+
+            int femtioLappar = växelFemtioLappar / 50;
+            int växelTjugoLappar = växelFemtioLappar - (femtioLappar * 50);
+
+            int tjugoLappar = växelTjugoLappar / 20;
+            int växelTioKronor = växelTjugoLappar - (tjugoLappar * 20);
+
+            int tioKronor = växelTioKronor / 10;
+            int växelFemKronor = växelTioKronor - (tioKronor * 10);
+
+            int femKronor = växelFemKronor / 5;
+            int växelEnKronor = växelFemKronor - (femKronor * 5);
+
+            int enKronor = växelEnKronor / 1;
+
+            //OUTPUT
+            //Samtliga sparade valörer skrivs ut i varsin textbox. 
+            
+            if (femhundraLappar != 0)
+            {
+                tbxFemhundra.Text = "Femhundralappar: " + femhundraLappar.ToString();
+            }
+            if (tvåhundraLappar != 0)
+            {
+                tbxTvåhundra.Text = "Tvåhundralappar: " + tvåhundraLappar.ToString();
+            }
+            if (hundraLappar != 0)
+            {
+                tbxHundra.Text = "Hundralappar: " + hundraLappar.ToString();
+            }
+            if (femtioLappar != 0)
+            {
+                tbxFemtio.Text = "Femtilappar: " + femtioLappar.ToString();
+            }
+            if (tjugoLappar != 0)
+            {
+                tbxTjugo.Text = "Tjugolappar: " + tjugoLappar.ToString();
+            }
+            if (tioKronor != 0)
+            {
+                tbxTio.Text = "Tiokronor: " + tioKronor.ToString();
+            }
+            if (femKronor != 0)
+            {
+                tbxFem.Text = "Femkronor: " + femKronor.ToString();
+            }
+            if (enKronor != 0)
+            {
+                tbxEn.Text = "Enkronor: " + enKronor.ToString();
+            }
+
+        }
+
+        private void txbFemhundra_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txbPris_TextChanged(object sender, EventArgs e)
+        {
+
+            //Tömmar alla boxar.
+            tbxFemhundra.Text = "";
+            tbxTvåhundra.Text = "";
+            tbxHundra.Text = "";
+            tbxFemtio.Text = "";
+            tbxTjugo.Text = "";
+            tbxTio.Text = "";
+            tbxFem.Text = "";
+            tbxEn.Text = "";
+
+        }
+
+        private void txbBetalt_TextChanged(object sender, EventArgs e)
+        {
+
+            //Tömmar alla boxar.
+            tbxFemhundra.Text = "";
+            tbxTvåhundra.Text = "";
+            tbxHundra.Text = "";
+            tbxFemtio.Text = "";
+            tbxTjugo.Text = "";
+            tbxTio.Text = "";
+            tbxFem.Text = "";
+            tbxEn.Text = "";
+
+        }
+    }
+}
